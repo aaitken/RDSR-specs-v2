d1e5d38edf49d701f04500f3fa7bb3b7776d95fd
Alex Aitken <aaitken@alexaitken.net>
2014-10-16 12:29:37 -0600

    WIP

--- out/assets/scripts/app.js
+++ out/assets/scripts/app.js
@@ -1,4 +1,11 @@
 (function() {
+  var map;
 
+  map = L.map('ama-map').setView([51.505, -0.09], 13);
+
+  L.tileLayer('https://a.tiles.mapbox.com/v4/8ken.jpg91pdd/page.html?access_token=pk.eyJ1IjoiOGtlbiIsImEiOiJvZ0lMWU9NIn0.AWbPDeAb1PbKgtoBzk-_Vg#4/39.91/-105.01', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18
+  }).addTo(map);
 
 }).call(this);
